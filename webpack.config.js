8c046bf2daeff47fe35063fda220f2cf70071c79
Tsung Hung <hungmasterj@gmail.com>
2016-05-16 00:08:10 -0700

    added scss and img loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,37 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
+
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
+
   module: {
-    loaders: [{
+    loaders: [
+      {
       exclude: /node_modules/,
       loader: 'babel'
-    }]
+    },
+    {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+    },
+    {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'url?limit=8192',
+            'img'
+        ]
+    }
+    ]
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+
   devServer: {
     historyApiFallback: true,
     contentBase: './'
