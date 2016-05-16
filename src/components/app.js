bf68e8c2ab5e8716c6bdaa61cf9a9bf73d5766a4
Tsung Hung <hungmasterj@gmail.com>
2016-05-15 22:55:37 -0700

    first commit;

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,9 @@ import { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>Welcome to Reduxilus!</h1>
+      </div>
     );
   }
 }
