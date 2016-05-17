a3170fc8bad6bcc20f570ead633fcdfe27795653
Tsung Hung <hungmasterj@gmail.com>
2016-05-17 15:02:28 -0700

    abstracted out the search video element and added lodash, created delayed, and also improved the searchbar functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './style/style.scss';
 
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/searchBar';
 import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
 
 const API_KEY = 'AIzaSyBs3VQE2nFE3d4G655bavpQMPdXweit7T0';
 
@@ -14,21 +16,37 @@ class App extends Component {
     super(props);
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
 
-    YTSearch({key: API_KEY, term: 'dr. strange'}, (videos) => {
-      this.setState({videos});
-      console.log(this.state.videos);
+    this.searchVideo('cute dogs');
+  }
+
+  searchVideo(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      console.log(videos);
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     })
   }
 
   render () {
+    // the function below delays the search function by 700ms
+    const searchVideo = _.debounce((term) => {
+      this.searchVideo(term)
+    }, 700);
+
     return (
-      <div>
-        <h1>Welcome to Reduxilus</h1>
-        <SearchBar />
-        <VideoList videos = {this.state.videos} />
+      <div className="row">
+        <h1 className="text-center  ">Welcome to Reduxilus</h1>
+        <SearchBar onSearchTermChange={searchVideo} />
+        <VideoDetail video={this.state.selectedVideo }/>
+        <VideoList
+            onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+            videos = {this.state.videos} />
       </div>
     )
   }
