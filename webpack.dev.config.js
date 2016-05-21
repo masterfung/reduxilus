d2c44da69e03cf6c219e11a3d88ba6b655005c10
Tsung Hung <hungmasterj@gmail.com>
2016-05-21 11:44:09 +0700

    renamed webpack to have dev

---
+++ webpack.dev.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.scss?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
