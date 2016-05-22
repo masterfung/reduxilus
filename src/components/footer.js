4acc73b9da78ce7f7ce8bd61b02faa803db47286
Tsung Hung <hungmasterj@gmail.com>
2016-05-22 23:01:41 +0900

    added footer

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="text-muted text-center">
+          <a href="https://github.com/masterfung/reduxilus">Reduxilus: A Project by Tsung Hung</a>
+
+          </p>
+      </div>
+  </footer>
+  )
+};
+
+export default Footer;
